785013d2514ff7b9672812f46c36e4367d32e7c3
Sparkey00 <Spark.vandals@gmail.com>
2020-08-03 00:04:50 +0300

    header layout

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,43 @@
+<header class="blog-header py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+        <div class="col-4 pt-1">
+            <a class="text-muted" href="#">Subscribe</a>
+        </div>
+        <div class="col-4 text-center">
+            <a class="blog-header-logo text-dark" href="{{ url('/') }}">{{ config('app.name', 'EasyCV2') }}</a>
+        </div>
+        <div class="col-4 d-flex justify-content-end align-items-center">
+            <ul class="navbar-nav ml-auto nav d-flex justify-content-between">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Войти') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Реестрация') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Выйти') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</header>
